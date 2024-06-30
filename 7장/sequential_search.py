f7e38c9828218e354e6ffdad9c2964c58f6091ca
Joshuasmile69 <joshuasmile69@gmail.com>
2024-06-30 12:52:03 +0900

    12장까지 내용 추가 & 2장 오류 수정 & 주석 수정

--- 7장/sequential_search.py
+++ 7장/sequential_search.py
@@ -1,5 +1,4 @@
 #순차탐색 알고리즘
-<<<<<<< HEAD
 #필요한 변수: 리스트 종류(a), 찾고자 하는 값(x)
 def sequential_search(a, x):
     n=len(a)
@@ -12,20 +11,6 @@ def sequential_search(a, x):
  
     #다 찾았는데도 없으면 none을 return        
     return "none"
-=======
-def sequential_search(a, x):
-#필요한 변수: 리스트 종류(a), 찾고자 하는 값(x)
-    n=len(a)
-    for i in range(0, n):
-    #첫째항부터 마지막 항까지
-        if a[i]==x:
-        #i번째 항이 찾고자 하는 값 x와 같은 값이라면
-            return i
-            #i를 return
-        
-    return "none"
-    #다 찾았는데도 없으면 none을 return
->>>>>>> 36d1208e825c7fa79e71da58b130749864dc8d0a
 
 a=[5, 3, 23, 2, 1, 6, 7, 8, 13, 11, 83, 9]
 
